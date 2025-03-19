27357a1f3ba916b39aed29689919335c13e4e7b2
Shamilvm <shanushamil086@gmail.com>
2025-03-19 22:47:11 +0530

    remove old files and create a backend setup

--- src/const/index.jsx
+++
@@ -1,6 +0,0 @@
-export const baseUrl = 'https://api.themoviedb.org/3';
-export const API_KEY = "00fd330291ef16430c1a55c4eb3c906c";
-export const imageUrl = 'https://image.tmdb.org/t/p/original';
-
-export const originals =`discover/tv?api_key=${API_KEY}&with_networks=213`
-export const action = `discover/movie?api_key=${API_KEY}&with_genres=28`
