1fa6040e8db9091dbbfca0dde9008515b9578097
Shamilvm <shanushamil086@gmail.com>
2025-03-20 12:37:30 +0530

    fetching movies and tv shows from tmdb

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
 import express from "express";
 import authRoutes from "./routes/authRoute.js";
+import movieRoutes from "./routes/movieRoute.js";
+import tvRoutes from "./routes/tvRoute.js";
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
+import { protectRoute } from "./middleware/protectRoute.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 const PORT = ENV_VARS.PORT;
 
 app.use(express.json()); // will allow us to parse req.body
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/movie", protectRoute, movieRoutes);
+app.use("/api/tv", protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
